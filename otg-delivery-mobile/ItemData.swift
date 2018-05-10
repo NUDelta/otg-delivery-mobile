0b5c09e301273c9af2561a39ff1572fd5ff388eb
Maggie <margaretlou2019@u.northwestern.edu>
2018-05-09 19:06:38 -0500

    Fix order description line wrapping

--- otg-delivery-mobile/ItemData.swift
+++ otg-delivery-mobile/ItemData.swift
@@ -19,17 +19,17 @@ struct Item {
 }
 
 let drinkData = [
-    Item(name: "Chicken Tinga Burrito", description: "Chicken sauteed with cabbage, onion, and chipotle sauce", price: 6.50),
+    Item(name: "Chicken Tinga Burrito", description: "Chicken sauteed with cabbage, onion, and chipotle sauce.", price: 6.50),
     Item(name: "Yucca with Chimichuri Burrito", description: "Served with mashed Cuban beans and rice.", price: 6.50),
-    Item(name: "Grilled Chicken Burrito", description: "Served with spicy rub and two-chili relish", price: 6.50),
-    Item(name: "Chochinita Pibil Burrito", description: "Pork cooked in fresh-squeezed orange and lime juice and achiote wrapped in plantain leaves", price: 6.50),
-    Item(name: "Sweet Potatoes Burrito", description: "Carrots, caulifower, and charred corn with guajillo sauce", price: 6.50),
+    Item(name: "Grilled Chicken Burrito", description: "Served with spicy rub and two-chili relish.", price: 6.50),
+    Item(name: "Chochinita Pibil Burrito", description: "Pork cooked in fresh-squeezed orange and lime juice and achiote wrapped in plantain leaves.", price: 6.50),
+    Item(name: "Sweet Potatoes Burrito", description: "Carrots, caulifower, and charred corn with guajillo sauce.", price: 6.50),
     Item(name: "Ground Beef Burrito", description: "Served with chili chipotle.", price: 6.50),
     Item(name: "Black Beans with Grilled Queso Burrito", description: "Pico de gallo and salso verde.", price: 6.50),
-    Item(name: "Tofu Burrito", description: "Made from Tofu and served to steaming perfection", price: 6.50),
+    Item(name: "Tofu Burrito", description: "Made from Tofu and served to steaming perfection.", price: 6.50),
     Item(name: "Caramelized Onions and Charred Poblano Burrito", description: "Red peppers with chihuahua cheese.", price: 6.50),
     Item(name: "Panko-Habanero Crusted Tilapia Burrito", description: "Served with pineapple mojo.", price: 6.50),
-    Item(name: "Spicy Pork with Salsa Verde Burrito", description: "With salsa verde", price: 6.50),
+    Item(name: "Spicy Pork with Salsa Verde Burrito", description: "With salsa verde.", price: 6.50),
     Item(name: "Al Pastor Burrito", description: "Marinated pork.", price: 6.50),
     Item(name: "Carne Asada Burrito", description: "Spiced-rubbed skirt steak.", price:6.50)
 ]

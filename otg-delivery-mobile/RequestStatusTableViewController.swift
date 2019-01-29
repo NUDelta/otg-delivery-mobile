1b86ced4ed3b7ad714e912f516c08fc268ebd28f
Maggie <margaretlou2019@u.northwestern.edu>
2019-01-29 10:53:56 -0600

    Remove unnecessary code in RequestStatusController - redundant with OrderViewController

--- otg-delivery-mobile/RequestStatusTableViewController.swift
+++ otg-delivery-mobile/RequestStatusTableViewController.swift
@@ -13,51 +13,6 @@ class RequestStatusTableViewController: UITableViewController {
     var myRequests = [CoffeeRequest]()
     @IBOutlet weak var MyRequestsTableView: UITableView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.register(RequestStatusTableViewCell.self, forCellReuseIdentifier: RequestStatusTableViewCell.reuseIdentifier)
-
-        User.getMyRequests(completionHandler: { coffeeRequests in
-            DispatchQueue.main.async {
-                self.myRequests = coffeeRequests
-                self.MyRequestsTableView.reloadData()
-            }
-        })
-    }
+    // Cell configuration implemented in OrderViewController
     
-    
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: RequestStatusTableViewCell.reuseIdentifier, for: indexPath) as? RequestStatusTableViewCell else {
-            fatalError("Couldn't dequeue RequestStatusTableViewCell")
-        }
-        
-        print("Panda")
-        
-        let request = myRequests[indexPath.row]
-
-        cell.statusDetailsLabel.text = request.status
-        cell.expirationDetailsLabel.text = request.endTime
-        cell.deliveryLocationDetailsLabel.text = CoffeeRequest.arrayToJson(arr: request.deliveryLocation)
-        cell.pickupLocationDetailsLabel.text =  request.endTime
-        cell.specialRequestsDetailsLabel.text = request.deliveryLocationDetails
-        
-        // Text wraps
-        cell.orderLabel.numberOfLines = 0
-        cell.statusDetailsLabel.numberOfLines = 0
-        cell.expirationDetailsLabel.numberOfLines = 0
-        cell.deliveryLocationDetailsLabel.numberOfLines = 0
-        cell.pickupLocationDetailsLabel.numberOfLines = 0
-        cell.specialRequestsDetailsLabel.numberOfLines = 0
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.myRequests.count
-    }
-    
-    func completeOrderPressed(sender: UIButton!){
-        print("Complete order pressed.")
-    }
 }

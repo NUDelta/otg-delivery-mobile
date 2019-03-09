830cdfb892b7016479afbbce3fb170c8a8d98d57
Maggie <margaretlou2019@u.northwestern.edu>
2019-03-09 12:15:45 -0600

    Add separate folders for organization

---
+++ otg-delivery-mobile/DataModels/Item.swift
@@ -0,0 +1,101 @@
+//
+//  DrinkData.swift
+//  otg-delivery-mobile
+//
+//  Created by Sam Naser on 4/4/18.
+//  Copyright © 2018 Sam Naser. All rights reserved.
+//
+
+import Foundation
+
+struct Item : Codable {
+    private static let apiUrl: String = Constants.apiUrl + "items"
+    
+    enum CodingKeys : String, CodingKey {
+        case id = "_id"
+        case name
+        case description
+        case price
+        case location
+    }
+    
+    let id: String
+    let name: String
+    let description: String
+    let price: Double
+    let location: String
+}
+
+extension Item {
+    func getPriceString() -> String {
+        return String.init(format: "$%.2f", self.price)
+    }
+    
+    static func get(withId id: String, completionHandler: @escaping (Item?) -> Void) {
+        print("Get item with id \(id)")
+        
+        let url = URL(string: "\(Item.apiUrl)/\(id)")
+        let session: URLSession = URLSession.shared
+        let requestURL = URLRequest(url: url!)
+        
+        let task = session.dataTask(with: requestURL){ data, response, error in
+            guard let data = data else {
+                return
+            }
+            
+            print("ITEM: get with id \(id)")
+            
+            var item: Item?
+            let httpResponse = response as? HTTPURLResponse
+            
+            if(httpResponse?.statusCode != 400){
+                do {
+                    let decoder = JSONDecoder()
+                    item = try decoder.decode(Item.self, from: data)
+                } catch {
+                    print("ITEM.get: error trying to convert data to JSON...")
+                    print(error)
+                }
+            }
+            completionHandler(item!)
+        }
+        
+        task.resume()
+    }
+    
+    static func getAll(forLocation location: String, completionHandler: @escaping ([Item]) -> Void) {
+        print("Get all items for location \(location)")
+        var components = URLComponents(string: "")
+        components?.queryItems = [
+            URLQueryItem(name: "location", value: location)
+        ]
+        
+        let url = URL(string: "\(Item.apiUrl)?location=\(location)")
+        let session: URLSession = URLSession.shared
+        let requestURL = URLRequest(url: url!)
+
+        
+        let task = session.dataTask(with: requestURL){ data, response, error in
+            print("ITEM MODEL: Getting items for \(location)")
+            guard let data = data else {
+                return
+            }
+            
+            var items: [Item] = []
+            let httpResponse = response as? HTTPURLResponse
+            
+            if(httpResponse?.statusCode != 400){
+                do {
+                    let decoder = JSONDecoder()
+                    items = try decoder.decode([Item].self, from: data)
+                } catch {
+                    print("ITEM: error trying to convert data to JSON...")
+                    print(error)
+                }
+            }
+            completionHandler(items)
+        }
+        
+        task.resume()
+    }
+}
---
+++ otg-delivery-mobile/SubmitRequestControllers/RequestConfirmationViewController.swift
@@ -0,0 +1,119 @@
+//
+//  RequestConfirmationViewController.swift
+//  otg-delivery-mobile
+//
+//  Created by Maggie Lou on 2/26/19.
+//  Copyright © 2019 Sam Naser. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class RequestConfirmationViewController: UIViewController {
+    
+    var currentRequest: CoffeeRequest?
+
+    @IBOutlet weak var itemLabel: UILabel!
+    @IBOutlet weak var startTimeLabel: UILabel!
+    @IBOutlet weak var endTimeLabel: UILabel!
+    @IBOutlet weak var meetingPointsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if currentRequest == nil {
+            let alert = UIAlertController(title: "We apologize. There is some error with your order.", message: "", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+                let mainPage: OrderViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "mainOrderViewController") as! OrderViewController
+                
+                self.present(mainPage, animated: true, completion: nil)
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        itemLabel.text = currentRequest!.item
+        startTimeLabel.text = CoffeeRequest.parseTime(dateAsString: currentRequest!.orderStartTime)
+        endTimeLabel.text = CoffeeRequest.parseTime(dateAsString: currentRequest!.orderEndTime)
+        
+        meetingPointsLabel.text = CoffeeRequest.prettyParseArray(arr: (currentRequest?.deliveryLocationOptions)!)
+        
+
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        let prevPage: MeetingPointTableViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MeetingPointTableViewController") as! MeetingPointTableViewController
+        prevPage.currentRequest = currentRequest
+        self.present(prevPage, animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        let mainPage: OrderViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "mainOrderViewController") as! OrderViewController
+        
+        self.present(mainPage, animated: true, completion: nil)
+    }
+    
+    @IBAction func submitButton(_ sender: Any) {
+        currentRequest = setTimeProbabilities(request: currentRequest!)
+        CoffeeRequest.postCoffeeRequest(coffeeRequest: currentRequest!)
+        
+        let defaults = UserDefaults.standard
+        var requestsPlaced = defaults.object(forKey: "requestsPlaced") as! Int + 1
+        defaults.set(requestsPlaced, forKey: "requestsPlaced")
+        
+        // Set up notifications
+        let secondsNowToStartTime = (CoffeeRequest.stringToDate(s: currentRequest!.orderStartTime)).timeIntervalSinceNow
+        
+        // Set up request acceptance notification
+        let seconds1hr15min = 60 * 1//60 * 75
+        let secondsNowToAcceptanceTime =
+            Int(secondsNowToStartTime) + seconds1hr15min
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(secondsNowToAcceptanceTime), execute: {
+            User.sendNotification(deviceId: Constants.myUserId, message: "Send \(defaults.object(forKey: "username")) notification that their request has been accepted. Tell DTR helper to be ready in 15 min and to choose a meeting point.")
+        })
+        
+        // Set up text user notification
+        let seconds1hr23min = 60 * 3//60 * 83
+        let secondsNowToTextTime = Int(secondsNowToStartTime) + seconds1hr23min
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(secondsNowToTextTime), execute: {
+            User.sendNotification(deviceId: Constants.myUserId, message: "Text user \(String(describing: defaults.object(forKey: "username")))")
+        })
+        
+        // Set up helper arriving soon notification
+        // Set up text user notification
+        let seconds1hr25min = 60 * 5//60 * 85
+        let secondsNowToArrivalNotification = Int(secondsNowToStartTime) + seconds1hr25min
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(secondsNowToArrivalNotification), execute: {
+            User.sendNotification(deviceId: defaults.object(forKey:"tokenId") as! String, message: "Your helper is arriving soon! Please go meet them at the meeting point now.")
+            User.sendNotification(deviceId: Constants.myUserId, message: "Tell DTR helper to head to meeting point now to meets.")
+        })
+    }
+
+    
+    func setTimeProbabilities(request: CoffeeRequest) -> CoffeeRequest {
+        let defaults = UserDefaults.standard
+        let requestsPlaced = defaults.object(forKey: "requestsPlaced") as! Int
+        
+        if (requestsPlaced < 1) {
+            request.timeProbabilities[1] = "30%"
+            request.timeProbabilities[3] = "60%"
+        } else {
+            request.timeProbabilities[1] = "60%"
+            request.timeProbabilities[3] = "30%"
+        }
+        
+        return request
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

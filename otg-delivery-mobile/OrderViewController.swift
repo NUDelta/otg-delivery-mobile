a3cd3b78afc2c8a5fc73fd31f015dd2e6fd5ff10
Maggie <margaretlou2019@u.northwestern.edu>
2019-03-09 12:14:48 -0600

    Only allow requests between 11-5

--- otg-delivery-mobile/OrderViewController.swift
+++ otg-delivery-mobile/OrderViewController.swift
@@ -22,8 +22,18 @@ class OrderViewController: UIViewController, CLLocationManagerDelegate, UITableV
     
     //On plus sign pressed
     @IBAction func createNewOrder() {
-        self.currentActionType = .Order
-        self.performSegue(withIdentifier: "orderFormSegue", sender: self)
+        // Check time - only open 11AM - 5PM
+        if (checkDeliveryAvailabilityTimeframe()) {
+            self.currentActionType = .Order
+            self.performSegue(withIdentifier: "orderFormSegue", sender: self)
+        } else {
+            let alert = UIAlertController(title: "You can only submit requests between 11AM - 5PM each day.", message: "", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
     }
     
     public static let sharedManager = OrderViewController()
@@ -369,6 +379,15 @@ class OrderViewController: UIViewController, CLLocationManagerDelegate, UITableV
         default:
             break;
         }
+    }
+    
+    func checkDeliveryAvailabilityTimeframe() -> Bool {
+        let now = NSDate()
+        let nowDateValue = now as Date
+        let todayAt11AM = Calendar.current.date(bySettingHour: 11, minute: 0, second: 0, of: nowDateValue)
+        let todayAt5PM = Calendar.current.date(bySettingHour: 17, minute: 0, second: 0, of: nowDateValue)
+        
+        return nowDateValue >= todayAt11AM! && nowDateValue <= todayAt5PM!
     }
 }
 

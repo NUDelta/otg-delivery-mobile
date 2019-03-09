d01c9b2027aa9a7a842a37dbf786f2a231aa60ef
Maggie <margaretlou2019@u.northwestern.edu>
2019-03-09 12:13:51 -0600

    Move models into a folder

--- otg-delivery-mobile/Constants.swift
+++ otg-delivery-mobile/Constants.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 struct Constants {
-    //static let apiUrl: String = "https://otg-delivery.herokuapp.com/"
-    static let apiUrl: String = "http://localhost:8080/"
-    static let myUserId: String = "1549238CDD7A8E2484691CC09AB204EA8EFF6F1CC6BC934D087AAFDEAB5BE435"
+    static let apiUrl: String = "https://otg-delivery.herokuapp.com/"
+    //static let apiUrl: String = "http://localhost:8080/"
+    static let myUserId: String = "11ECDC14490132103C3ED6F62CA0FF20722D812BE63434B98693AB95317A5F57"
     
 }

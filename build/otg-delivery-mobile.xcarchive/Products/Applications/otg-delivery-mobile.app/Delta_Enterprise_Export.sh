5ddb461256cd06924daf134059523f41c0c96ba7
Cooper Barth <cooperfbarth@gmail.com>
2019-05-01 16:24:16 -0500

    removed big files

--- build/otg-delivery-mobile.xcarchive/Products/Applications/otg-delivery-mobile.app/Delta_Enterprise_Export.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# original source from http://www.thecave.com/2014/09/16/using-xcodebuild-to-export-a-ipa-from-an-archive/
-
-xcodebuild clean -project Zombies\ Interactive -configuration Release -alltargets
-xcodebuild archive -project Zombies\ Interactive.xcodeproj -scheme Zombies\ Interactive -archivePath Zombies\ Interactive.xcarchive
-
-xcodebuild -exportArchive -archivePath Zombies\ Interactive.xcarchive -exportOptionsPlist zombies_export.plist -exportPath zombies.ipa
